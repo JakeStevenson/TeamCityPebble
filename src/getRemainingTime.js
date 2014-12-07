410439c7b448c28e57611f9b6a92d9c66e635fc1
Jake Stevenson <jakestevenson@gmail.com>
2014-12-07 09:38:48 -0600

    Maybe bad exports

--- src/getRemainingTime.js
+++ src/getRemainingTime.js
@@ -5,7 +5,7 @@ var parseString = require('xml2js').parseString;
 console.log("Loaded getTimeRemaining");
 
 var timeChecker = exports;
-exports.getTimeRemaining = function(buildId){
+timeChecker.getTimeRemaining = function(buildId){
 		//Sample of AJAX request that'll give us times remaining for current builds -- /ajax.html?getRunningBuilds=1
 		var sampleXML = ' <response><build buildId="23624" buildTypeId="bt2" text="build.proj.teamcity" successful="true" buildTypeSuccessful="true" showAsExceeded="false" showOvertimedIcon="false" showTimeLeft="true" totalEstimate="20m:58s" remainingTime="16m:02s" exceededEstimatedDurationTime="&lt; 1s" elapsedTime="5m:34s" completedPercent="26" hasArtifacts="false" /></response>';
 
