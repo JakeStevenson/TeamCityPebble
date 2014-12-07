e3856cd11e955390a279d0ffedc551fac7b50627
Jake Stevenson <jakestevenson@gmail.com>
2014-12-07 09:44:32 -0600

    Missed this

--- src/app.js
+++ src/app.js
@@ -32,7 +32,6 @@ main.on('click', 'up', function() {
     }]
   });
   menu.on('select', function(e) {
-	console.dir(timeRemaining);
 	timeRemaining.getTimeRemaining(23624).then(function(time){
 		console.log('Time shown:' + time);
 	});
